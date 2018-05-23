25420d70db481b93f7998866bbeffb5eaf5478ae
Aditya Ramani <adityaramani@hotmail.com>
2018-05-23 17:55:43 +0530

    initial commit

---
+++ expensive_functions.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def create_matrix(n,seed):
+    A = []
+    for i in range(n):
+        temp = []
+        for j in range(n):
+            temp.append(i+j+seed)
+        A.append(temp)
+
+    return np.array(A)
+    
+
+def function_one(x,**kargs):
+    total = 0
+    for i in range(x):
+        total += i**i
+    
+    return total%2
+
+def function_two(x,**kargs):
+    if kargs['seed'] == None:
+        seed = 1
+    else:
+        seed = kargs['seed']
+    A = create_matrix(x,seed)
+    B = create_matrix(x,seed)
+    
+    print(A.shape)
+    return 1
---
+++ main.py
@@ -0,0 +1,62 @@
+from optimizer import optimize_comprehensions
+import numpy as np
+import matplotlib.pyplot as plt
+import timeit
+from functools import partial
+from time import sleep
+import random
+from expensive_functions import *
+
+
+def list_comprehension(expensive_func,a,**kargs):
+    return [
+        expensive_func(x,**kargs)
+        for x in [a]
+        if expensive_func(x,**kargs)
+    ]
+
+@optimize_comprehensions
+def list_comprehension__optimized(expensive_func,a,**kargs):
+    return [
+        expensive_func(x,**kargs)
+        for x in [a]
+        if expensive_func(x,**kargs)
+    ]
+func_times = [x for x in range(900, 1000)]
+
+opt_comp_time_frac = []
+opt_time =[]
+cmp_time = []
+
+
+expensive_func = function_one
+
+# expensive_func = function_two
+for i in func_times:
+    seed = random.random()*10000
+    print("I= ", i,"seed = " ,seed)
+    print("Running Non Optimized")
+    comp_time = timeit.timeit(
+    partial(list_comprehension, expensive_func, i,seed=seed),
+        number=5,
+    )
+    cmp_time.append(comp_time)
+    print("Running Optimized")
+    optimized_comp_time = timeit.timeit(
+        partial(list_comprehension__optimized, expensive_func,i,seed=seed),
+        number=5,
+    )
+    opt_time.append(optimized_comp_time)
+
+    #opt_comp_time_frac.append(optimized_comp_time / comp_time)
+    print("Optimized = ", optimized_comp_time , "Non opti = ", comp_time,"\n")
+
+with plt.rc_context({'axes.edgecolor':'gray', 'xtick.color':'gray', 'ytick.color':'gray', 'figure.facecolor':'white'}):
+    # plt.plot(func_times, opt_comp_time_frac)
+    plt.plot(func_times, opt_time, color='r')
+    plt.plot(func_times, cmp_time,color='y')
+    
+    
+    plt.ylabel('Execution time of optimized Time and original Time',  fontsize=10, wrap=True)
+    plt.xlabel('Execution time of function (seconds)', fontsize=10)
+    plt.savefig('Blah.svg')
